e83f32bea6fe3be8bebecaf1ac2fd7bfed17e280
Cindy Zhang <cixzhang@gmail.com>
2016-09-09 17:56:48 -0700

    day and night lighting

--- src/systems/lighting.js
+++ src/systems/lighting.js
@@ -3,8 +3,46 @@ var PIXI = require('pixi.js');
 
 var stage;
 var filter;
-var count = 0;
 var lastTime;
+
+var markers = [
+  {
+    time: 1 * 60, // 1AM
+    matrix: [
+      0.2, 0, 0, 0, 0,
+      0, 0.2, 0, 0, 0,
+      0, 0, 0.6, 0, 0,
+      0, 0, 0, 1, 0
+    ]
+  },
+  {
+    time: 7 * 60, // 7AM
+    matrix: [
+      1, 0, 0, 0, 0,
+      0, 0.5, 0, 0, 0,
+      0, 0, 0.75, 0, 0,
+      0, 0, 0, 1, 0
+    ]
+  },
+  {
+    time: 13 * 60, // 1PM
+    matrix: [
+      1, 0, 0, 0, 0,
+      0, 1, 0, 0, 0,
+      0, 0, 0.9, 0, 0,
+      0, 0, 0, 1, 0
+    ]
+  },
+  {
+    time: 21 * 60, // 7 PM
+    matrix: [
+      0.5, 0, 0, 0, 0,
+      0, 0.5, 0, 0, 0,
+      0, 0, 0.8, 0, 0,
+      0, 0, 0, 1, 0
+    ]
+  }
+];
 
 function setup(container) {
   stage = container;
@@ -16,12 +54,34 @@ function update(gametime) {
   if (lastTime != null && gametime.minutes === lastTime) {
     return;
   }
-
-  count += 0.1;
-  filter.hue(count);
-  console.log(gametime.hour + ':' + gametime.minute);
-
+  filter.matrix = timeToMatrix(gametime.hour * 60 + gametime.minute);
   lastTime = gametime.minutes;
+}
+
+function timeToMatrix(minutes) {
+  var last = markers[markers.length - 1];
+  var next = markers[0];
+
+  markers.forEach(function (marker) {
+    if (minutes > marker.time) {
+      last = marker;
+    }
+
+    if (minutes < marker.time && next === marker[0]) {
+      next = marker;
+    }
+  });
+
+  var amt = (minutes - last.time) / (next.time - last.time);
+  return interpolate(last.matrix, next.matrix, amt);
+}
+
+function interpolate(matrixA, matrixB, amt) {
+  return matrixA.map(function (valA, i) {
+    var valB = matrixB[i];
+    var delta = valB - valA;
+    return valA + delta * amt;
+  });
 }
 
 module.exports = {
